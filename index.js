b2f814f64948bc9e7fc342e5f4fa5f624fb4df06
Uday2902 <udaykpatel2004@gmail.com>
2025-01-30 15:12:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const { events } = require("./events");
+const { allEventsData } = require("./allEventsData");
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+
+
+app.get('/events', (req, res) => {
+    // console.log(events)
+    res.json(events)
+})
+
+app.get('/participants', (req, res) => {
+    // console.log(allEventsData)
+    res.json(allEventsData)
+})
+
+app.listen(5000, () => {
+    console.log("Server is listening at port - 5000");
+})
