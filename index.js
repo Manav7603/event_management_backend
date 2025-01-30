4fc8437a85b8bed1eb30ea21773655d24c96c270
Uday2902 <udaykpatel2004@gmail.com>
2025-01-30 15:50:24 +0530

    new domain added in cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { allEventsData } = require("./allEventsData");
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://eventlyworld.vercel.app/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
